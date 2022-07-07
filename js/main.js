5b5f3039e7cdd5dc9ddae289b3eab8c00ab53178
tuesdayy96 <104114353+tuesdayy96@users.noreply.github.com>
2022-07-07 18:09:53 +0900

    release1.0

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    // setInterval(function(){
+    //     $('.card_A').stop().animate({marginLeft: -240},function(){
+    //         $('.item:nth-child(2)').addClass('on');
+    //         $('.item:nth-child(2)').siblings().removeClass('on');
+    //         $('.item:first').appendTo('.card_A');
+    //         $('.card_A').css({marginLeft:0});
+    //     });
+    // },5000)
+})
